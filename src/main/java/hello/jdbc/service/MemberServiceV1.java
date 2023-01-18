00951ed1f6cb0f7c4122d53cde6b6132b379617b
jvnlee <lhjun1028@naver.com>
2023-01-18 15:40:12 +0900

    feat: Member간 이체 기능이 담긴 MemberServiceV1 추가
    MemberServiceV1은 트랜잭션 적용이 안된 버전으로, 수신자 ID가 ex인 경우 예외가 발생하도록 만들어짐. 따라서 테스트 케이스 중에서 이체 도중 예외가 발생하는 시나리오에서는 송금자인 MemberA의 돈만 빠져나가고 수금자인 MemberB의 돈은 그대로인 문제 상황이 연출됨.

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,31 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void transfer(String senderId, String receiverId, int money) throws SQLException {
+        Member sender = memberRepository.findById(senderId);
+        Member receiver = memberRepository.findById(receiverId);
+
+        memberRepository.update(senderId, sender.getMoney() - money);
+        validation(receiver);
+        memberRepository.update(receiverId, receiver.getMoney() + money);
+    }
+
+    private void validation(Member receiver) {
+        if (receiver.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 작업 도중 예외 발생!");
+        }
+    }
+
+}
