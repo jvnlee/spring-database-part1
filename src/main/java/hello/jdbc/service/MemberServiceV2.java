49792d0c55721c619d418cf9c4ba199bf8b58241
jvnlee <lhjun1028@naver.com>
2023-01-19 21:24:31 +0900

    fix: 불필요한 Service 어노테이션 제거
    해당 서비스 클래스들은 스프링 빈으로 등록해서 사용하려는 클래스들이 아니기 때문에 어노테이션을 안붙여도 됨.

--- src/main/java/hello/jdbc/service/MemberServiceV2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -5,13 +5,11 @@ import hello.jdbc.repository.MemberRepositoryV1;
 import hello.jdbc.repository.MemberRepositoryV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@Service
 @RequiredArgsConstructor
 @Slf4j
 public class MemberServiceV2 {
